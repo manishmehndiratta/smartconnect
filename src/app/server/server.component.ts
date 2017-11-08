1bfa747b8c94d92056b200f1244e30664afa83db
Mehndiratta <Manish.Mehndiratta@apachecorp.com>
2017-11-08 16:39:04 -0600

    server components added

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
+import { Server } from './server';
+import { ServerService } from '../services/server.service';
 
 @Component({
   selector: 'app-server',
@@ -8,10 +10,41 @@ import { ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ServerComponent implements OnInit {
+  servers: Array<Server>=[];
+  //servers: Server[];
+  serverForm: boolean=false;
+  editServerForm: boolean=false;
+  isNewForm: boolean;
+  newServer: any={};
+  editedServer: any = {};
+  envProd: string='Production';
+  envTest: string='Test';
+  envDev: string='Development';
 
   constructor() { }
 
   ngOnInit() {
+    this.getServers();
   }
 
+  getServers(){
+
+  }
+
+  showAddServerForm() {
+    // resets form if edited product
+    if(this.servers.length) {
+      this.newServer = {};
+    }
+    this.serverForm = true;
+    this.isNewForm = true;
+  }
+
+  saveServer(server: Server) {
+    if(this.isNewForm) {
+      // add a new product
+      //this._productService.addProduct(product);
+    }
+    this.serverForm = false;
+  }
 }
---
+++ src/app/services/server.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Server } from '../Server/server';
+
+@Injectable()
+export class ServerService{
+
+    addServer(server: Server){
+        //this.
+    }
+}
